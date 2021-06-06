25080b35d9508ff5b0540669066519f206ea9ffa
Jelena Feliciano <jfel217@gmail.com>
2021-06-05 22:26:06 -0400

    added tick and componentDidMOunt() functions

--- scoreboard/src/components/Stopwatch.js
+++ scoreboard/src/components/Stopwatch.js
@@ -2,13 +2,32 @@ import React, {Component} from 'react'
 
 class Stopwatch extends Component {
     state ={
-        isRunning: false
+        isRunning: false,
+        elapsedTime: 0,
+        previousTime: 0
+    }
+//function that starts or stops the timer depending on the isRunning state
+    handleStopWatch = () => {
+        this.setState( prevState => ({
+            isRunning: !this.state.isRunning //setting the isRunning state to its opposite stop/start
+        }));
+        if(!this.state.isRunning) {
+            this.setState({previousTime: Date.now() })
+        }
     }
 
-    handleStopWatch = () => {
-        this.setState({
-            isRunning: !this.state.isRunning
-        });
+    componentDidMount() {
+        this.intervalID = setInterval(() => this.tick(), 100)
+    }
+
+    tick = () => {
+        if(this.state.isRunning) {
+            const now = Date.now();
+            this.setState( prevState => ({
+                previousTime: now,
+                elapsedTime: this.state.elapsedTime + (now - this.state.previousTime)
+            }));
+        }
     }
 
     render() {
