6959238866786e525e23de44fc4818e17ae0c0e9
Jelena Feliciano <jfel217@gmail.com>
2021-06-08 11:22:05 -0400

    Update Player.js
    Made the Player component a PureComponent to prevent rerendering

--- scoreboard/src/components/Player.js
+++ scoreboard/src/components/Player.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import Counter from './Counter';
 
-const Player = (props) => {
+class Player extends PureComponent {
+  render() {
+  console.log(this.props.name + ' rendered ')
     return (
       <div className="player">
         <span className="player-name">
-          <button className="remove-player" onClick={() => props.removePlayer(props.id)}>x</button>
-          { props.name }
+          <button 
+          className="remove-player" 
+          onClick={() => this.props.removePlayer(this.props.id)}>x
+          </button>
+          { this.props.name }
         </span>
   
         <Counter 
-        score={props.score}
-        index={props.index}
-        changeScore={props.changeScore}
+        score={this.props.score}
+        index={this.props.index}
+        changeScore={this.props.changeScore}
         />
       </div>
     );
+  }
+  
   }
   export default Player
